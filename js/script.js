09272296fbca550e467f65c359730265d00d4bb6
UnnikrishnanNP12 <uknp0371@gmail.com>
2021-09-04 22:43:07 +0530

    comments added

--- js/script.js
+++ js/script.js
@@ -10,22 +10,25 @@ $(document).ready(function () {
     searchUserByFollower(follower)
 
 
+    // getting slider value
     $("#slider").change(function () {
         var follower = $("#slider").val()
         $("#follower").html("No of Follower: " + follower)
-
+        
         searchUserByFollower(follower)
-
+        
     })
-
+    
+    // showing minimum value enterd in slider
     $("#minimum").change(function () {
         minimumFollower = $("#minimum").val()
         $("#minimumFollowers").html("Minimum Follower: " + minimumFollower)
-
+        
         searchFollowers(minimumFollower,maximumFollower)
-
+        
     })
-
+    
+    // showing maximum value enterd in slider
     $("#maximum").change(function () {
         maximumFollower = $("#maximum").val()
         $("#maximumFollowers").html("Maximum Follower: " + maximumFollower)
@@ -34,20 +37,23 @@ $(document).ready(function () {
 
     })
 
+
+    // this function helps us to get users between a certain range
     function searchFollowers(minimumFollower, maximumFollower) {
         $("#results").empty()
         $.get("https://api.github.com/search/users?q=followers:" + minimumFollower + ".." + maximumFollower + "&per_page=100", function (data) {
             console.log(data)
             data.items.forEach(element => {
                 user = `<a target="_blank" href="${element.html_url}"><img class="img-thumbnail ml-5" width="150" height="100" src="${element.avatar_url}"/></a>`
-
+                
                 $("#results").append(user)
             });
             
         })
     }
-
-
+    
+    
+    // this function helps us to get the most followed in github
     function searchUserByFollower(follower) {
         $("#results").empty()
         $.get("https://api.github.com/search/users?q=followers:>=" + follower + "&per_page=100", function (data) {
