c07a697d3611f46e11405ae2447ef0c1ac23fc9c
UnnikrishnanNP12 <uknp0371@gmail.com>
2021-09-05 09:55:08 +0530

    added some styles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 // when document is ready
 $(document).ready(function () {
 
-    var minimumFollower = 0  , maximumFollower = 10000
+    var minimumFollower = 0  , maximumFollower = 100000
     var user = '' 
-
-
     searchUserByFollower()
+
+        // this function helps us to get the most followed in github
+        function searchUserByFollower() {
+            $("#results").empty()
+            $.get("https://api.github.com/search/users?q=followers:>=10000&per_page=100", function (data) {
+                
+                data.items.forEach(element => {
+                    user = `<a target="_blank" href="${element.html_url}">
+                            <img class="img-thumbnail ml-5" width="150" height="100" src="${element.avatar_url}"/>
+                            </a>`
+    
+                    $("#results").append(user)
+                });
+                
+            })
+        }
+    
+    })
     // showing minimum value enterd in slider
     $("#minimum").change(function () {
         minimumFollower = $("#minimum").val()
@@ -24,15 +40,15 @@ $(document).ready(function () {
 
     })
 
-
     // this function helps us to get users between a certain range
     function searchFollowers(minimumFollower, maximumFollower) {
         $("#results").empty()
+
         $("#text-change").html("Users with followers between " + minimumFollower + " and " + maximumFollower);
         $.get("https://api.github.com/search/users?q=followers:" + minimumFollower + ".." + maximumFollower + "&per_page=100", function (data) {
             console.log(data)
             data.items.forEach(element => {
-                user = `<a target="_blank" href="${element.html_url}">
+                user = `<a target="_blank" href="${element.html_url} style="padding:10px"">
                             <img class="img-thumbnail ml-5" width="150" height="100" src="${element.avatar_url}"/>
                             </a>
                             `
@@ -43,19 +59,3 @@ $(document).ready(function () {
         })
     }
     
-    
-    // this function helps us to get the most followed in github
-    function searchUserByFollower() {
-        $("#results").empty()
-        $.get("https://api.github.com/search/users?q=followers:>=10000&per_page=100", function (data) {
-            
-            data.items.forEach(element => {
-                user = `<a target="_blank" href="${element.html_url}"><img class="img-thumbnail ml-5" width="150" height="100" src="${element.avatar_url}"/></a>`
-
-                $("#results").append(user)
-            });
-            
-        })
-    }
-
-})
